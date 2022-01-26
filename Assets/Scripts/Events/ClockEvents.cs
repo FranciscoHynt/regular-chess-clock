ed5806bce66a5d62d6aa70a112fb616a4ac2d6ce
FranciscoHynt <0236432@feevale.br>
2022-01-26 10:35:30 -0300

    feat: start clock logic

---
+++ Assets/Scripts/Events/ClockEvents.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Events;
+
+namespace Events
+{
+    public static class ClockEvents
+    {
+        public static readonly StartClockEvent StartClockEvent = new StartClockEvent();
+    }
+
+    public class StartClockEvent : UnityEvent<StartClockEventData>{}
+    
+    public struct StartClockEventData
+    {
+        public int ClockTime { get; set; }
+        public int ExtraTime { get; set; }
+
+        public StartClockEventData(int clockTime, int extraTime)
+        {
+            this.ClockTime = clockTime;
+            this.ExtraTime = extraTime;
+        }
+    }
+}
