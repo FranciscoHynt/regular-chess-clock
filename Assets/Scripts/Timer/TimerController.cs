d106ea884a12520afe9a1a669e33345753883346
FranciscoHynt <0236432@feevale.br>
2022-01-26 10:58:34 -0300

    feat: added timer logic

--- Assets/Scripts/Timer/TimerController.cs
+++ Assets/Scripts/Timer/TimerController.cs
@@ -1,9 +1,55 @@
+using System;
+using System.Collections;
+using Events;
+using TMPro;
 using UnityEngine;
 
 namespace Timer
 {
     public class TimerController : MonoBehaviour
     {
-        
+        private TimeSpan clockTime;
+        private TextMeshProUGUI timerText;
+
+        private const string DATE_FORMAT = @"mm\:ss";
+
+        public TimerController()
+        {
+            ClockEvents.StartClockEvent.AddListener(StartTimer);
+        }
+
+        private void Awake()
+        {
+            timerText = GetComponentInChildren<TextMeshProUGUI>();
+        }
+
+        private void StartTimer(StartClockEventData data)
+        {
+            clockTime = TimeSpan.FromMinutes(data.ClockTime);
+
+            StartCoroutine(RunTimerRoutine());
+        }
+
+        private IEnumerator RunTimerRoutine()
+        {
+            WaitForSeconds wait = new WaitForSeconds(1);
+
+            while (true)
+            {
+                UpdateTimerText();
+                clockTime = GetClockTime();
+                yield return wait;
+            }
+        }
+
+        private TimeSpan GetClockTime()
+        {
+            return clockTime.Subtract(TimeSpan.FromSeconds(1));
+        }
+
+        private void UpdateTimerText()
+        {
+            timerText.text = clockTime.ToString(DATE_FORMAT);
+        }
     }
 }
