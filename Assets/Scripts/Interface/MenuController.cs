e55e5865cc93dec085b577c850472e3c2883c1ca
FranciscoHynt <0236432@feevale.br>
2022-01-30 23:24:17 -0300

    feat: added exit button

---
+++ Assets/Scripts/Interface/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Interface
+{
+    public class MenuController : MonoBehaviour
+    {
+        [SerializeField] private GameObject exitButton;
+
+        private void Start()
+        {
+            if (Application.platform != RuntimePlatform.WindowsPlayer)
+                exitButton.SetActive(false);
+        }
+
+        public void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
